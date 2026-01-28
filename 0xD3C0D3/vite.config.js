a0ee161e31814a429d711f8dbb1b99033840a3c3
hjasier <asiaurre@gmail.com>
2026-01-28 18:29:31 +0100

    update variables para despliege

--- 0xD3C0D3/vite.config.js
+++ 0xD3C0D3/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/0xD3C0D3-site/',
+  base: '/',
   plugins: [react(), tailwindcss()]
 })
